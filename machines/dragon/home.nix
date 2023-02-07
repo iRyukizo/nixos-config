7886bc67f39a094c8f3b681e4cb9c273358f6f99
Hugo Moreau <hugo.moreau@epita.fr>
2023-02-07 19:58:42 +0100

    machines: dragon: enable i3

--- machines/dragon/home.nix
+++ machines/dragon/home.nix
@@ -8,13 +8,9 @@
         wlan = "wlp3s0";
         eth = "enp60s0u1u3c2";
       };
-      urxvt.enable = true;
       zsh.enable = true;
-      dunst.enable = true;
-      flameshot.enable = true;
-      gtk.enable = true;
       xdg.enable = true;
-      rofi.enable = true;
+      i3.enable = true;
     };
   };
 }
