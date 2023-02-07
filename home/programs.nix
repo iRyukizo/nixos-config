d205d04ec2f6d38a8834ef4dfbec5bad97ce1280
Hugo Moreau <hugo.moreau@epita.fr>
2023-02-07 18:58:12 +0100

    home: urxvt: use xclip

--- home/programs.nix
+++ home/programs.nix
@@ -3,7 +3,6 @@
 {
   home.packages = with pkgs; [
     nixpkgs-fmt
-    xclip
     discord
 
     betterlockscreen
--- home/urxvt.nix
+++ home/urxvt.nix
@@ -32,6 +32,7 @@ in
     };
 
     home.packages = with pkgs; [
+      xclip
       perl
     ];
 
