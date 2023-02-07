2604aa5e14330688294bf0c479ce3e52c58562e3
Hugo Moreau <hugo.moreau@epita.fr>
2023-02-07 19:20:48 +0100

    home: add rofi configuration

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -1,27 +1,41 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  inherit (lib) mkDefault mkEnableOption mkIf;
+  cfg = config.my.home.rofi;
+in
 {
-  programs.rofi = {
-    enable = true;
+  options.my.home.rofi = {
+    enable = mkEnableOption "Home rofi configuration";
+  };
 
-    plugins = with pkgs; [
-      rofi-calc
-    ];
+  config = mkIf cfg.enable {
+    my.home = {
+      fonts.enable = mkDefault true;
+    };
 
-    font = "System San Francisco Display 18";
-    terminal = "${pkgs.zsh}/bin/zsh";
-    cycle = true;
-    location = "center";
-    theme = ./custom_nord.rasi;
+    programs.rofi = {
+      enable = true;
 
-    extraConfig = {
-      modi = "combi,drun,run,window,ssh,calc";
-      kb-remove-char-forward = "Delete";
-      kb-remove-char-back = "BackSpace,Shift+BackSpace";
-      kb-mode-next = "Control+Tab";
-      kb-mode-previous = "Control+h,Control+ISO_Left_Tab";
-      show-icons = false;
-      matching = "fuzzy";
+      plugins = with pkgs; [
+        rofi-calc
+      ];
+
+      font = "System San Francisco Display 18";
+      terminal = "${pkgs.zsh}/bin/zsh";
+      cycle = true;
+      location = "center";
+      theme = ./custom_nord.rasi;
+
+      extraConfig = {
+        modi = "combi,drun,run,window,ssh,calc";
+        kb-remove-char-forward = "Delete";
+        kb-remove-char-back = "BackSpace,Shift+BackSpace";
+        kb-mode-next = "Control+Tab";
+        kb-mode-previous = "Control+h,Control+ISO_Left_Tab";
+        show-icons = false;
+        matching = "fuzzy";
+      };
     };
   };
 }
