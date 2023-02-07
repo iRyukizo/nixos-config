98c981e744630da3d2e89640ba41e368ceac4394
Hugo Moreau <hugo.moreau@epita.fr>
2023-02-07 19:13:01 +0100

    home: add xdg configuration

--- home/xdg.nix
+++ home/xdg.nix
@@ -1,15 +1,25 @@
-{ ... }:
+{ config, lib, ... }:
 
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.my.home.xdg;
+in
 {
-  xdg.userDirs = {
-    enable = true;
-    createDirectories = true;
-    extraConfig = {
-      XDG_PERSO_DIR = "$HOME/PERSO";
-      XDG_EPITA_DIR = "$HOME/EPITA";
-      XDG_ASSIST_DIR = "$HOME/EPITA/ASSISTANTS";
-      XDG_LRDE_DIR = "$HOME/EPITA/LRDE";
-      XDG_SCROT_DIR = "$HOME/Pictures/screenshots";
+  options.my.home.xdg = {
+    enable = mkEnableOption "Home XDG configuration (personal directories)";
+  };
+
+  config = mkIf cfg.enable {
+    xdg.userDirs = {
+      enable = true;
+      createDirectories = true;
+      extraConfig = {
+        XDG_PERSO_DIR = "$HOME/PERSO";
+        XDG_EPITA_DIR = "$HOME/EPITA";
+        XDG_ASSIST_DIR = "$HOME/EPITA/ASSISTANTS";
+        XDG_LRDE_DIR = "$HOME/EPITA/LRDE";
+        XDG_SCROT_DIR = "$HOME/Pictures/screenshots";
+      };
     };
   };
 }
