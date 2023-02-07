6f04cdce6f0ab08e7040d356fa511f06ab2a3d34
Hugo Moreau <hugo.moreau@epita.fr>
2023-02-07 22:20:56 +0100

    home: add git configuration

--- home/git.nix
+++ home/git.nix
@@ -1,42 +1,88 @@
-{ ... }:
+{ config, lib, ... }:
 
-{
-  programs.git = {
-    enable = true;
+let
+  inherit (builtins) map;
+  inherit (lib) mkEnableOption mkIf mkOption types;
+  cfg = config.my.home.git;
 
-    userEmail = "hugo.moreau@epita.fr";
-    userName = "Hugo Moreau";
-
-    extraConfig = {
-      commit = { verbose = true; };
-      color = { ui = true; };
-      core = {
-        editor = "vim";
-        pager = "less -iXFR --mouse";
-      };
-      pager = { branch = false; };
-      init = { defaultBranch = "master"; };
-      push = { default = "simple"; };
-      pull = { rebase = true; };
+  emailCondition = types.submodule {
+    directory = {
+      type = types.str;
+      default = "~";
+      description = ''
+        Location to use.
+      '';
     };
 
-    includes = [
+    email = mkOption {
+      type = types.str;
+      description = ''
+        Email to use.
+      '';
+    };
+  };
+
+  makeConditions = emConds: (
+    map (emCond:
       {
-        condition = "gitdir:~/EPITA/LRDE";
+        condition = "gitdir:" + emCond.directory;
         contents = {
-          user = {
-            email = "hmoreau@lrde.epita.fr";
-          };
+          user.email = emCond.email;
         };
       }
-      {
-        condition = "gitdir:~/EPITA/ASSISTANTS";
-        contents = {
-          init = {
-            defaultBranch = "main";
-          };
+    ) emConds
+  );
+in
+{
+  options.my.home.git = {
+    enable = mkEnableOption "Home git configuration";
+    emailConditions = mkOption {
+      type = types.listOf emailCondition;
+      default = [ ];
+      description = ''
+        List of conditions for user.email.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.git = {
+      enable = true;
+
+      userEmail = "hugo.moreau@epita.fr";
+      userName = "Hugo Moreau";
+
+      extraConfig = {
+        commit = { verbose = true; };
+        color = { ui = true; };
+        core = {
+          editor = "vim";
+          pager = "less -iXFR --mouse";
         };
-      }
-    ];
+        pager = { branch = false; };
+        init = { defaultBranch = "master"; };
+        push = { default = "simple"; };
+        pull = { rebase = true; };
+      };
+
+      includes = [
+        {
+          condition = "gitdir:~/EPITA/LRDE";
+          contents = {
+            user = {
+              email = "hmoreau@lrde.epita.fr";
+            };
+          };
+        }
+        {
+          condition = "gitdir:~/EPITA/ASSISTANTS";
+          contents = {
+            init = {
+              defaultBranch = "main";
+            };
+          };
+        }
+      ] ++ makeConditions cfg.emailConditions;
+    };
   };
 }
