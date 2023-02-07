0f72cfd2153b7dd5650435ffd48e5072bd4d9785
Hugo Moreau <hugo.moreau@epita.fr>
2023-02-07 23:20:29 +0100

    modules: system: add default option

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,7 +1,25 @@
+{ config, lib, ... }:
+
+let
+  inherit (lib) mkDefault mkEnableOption mkIf;
+  cfg = config.my.system;
+in
 {
   imports = [
     ./crypt-system.nix
     ./nix.nix
     ./xserver.nix
   ];
+
+  options.my.system = {
+    enableDefault = mkEnableOption "Enable all default options";
+  };
+
+  config = mkIf cfg.enableDefault {
+    my.system = {
+      crypt.enable = mkDefault true;
+      nix.enable = mkDefault true;
+      xserver.enable = mkDefault true;
+    };
+  };
 }
