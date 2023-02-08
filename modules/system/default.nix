43fc70521714b60dd3ae5560604a587b1c807d13
Hugo Moreau <hugo.moreau@epita.fr>
2023-02-08 17:25:56 +0100

    modules: system: add basic configuration

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }:
 
 let
-  inherit (lib) mkDefault mkEnableOption mkIf;
+  inherit (lib) mkDefault mkEnableOption mkIf mkMerge;
   cfg = config.my.system;
 in
 {
@@ -19,19 +19,34 @@ in
 
   options.my.system = {
     enableDefault = mkEnableOption "Enable all default options";
+    enableBasic = mkEnableOption "Enable everything except graphic interface";
   };
 
-  config = mkIf cfg.enableDefault {
-    my.system = {
-      crypt.enable = mkDefault true;
-      docker.enable = mkDefault true;
-      gui.enable = mkDefault true;
-      locales.enable = mkDefault true;
-      networking.enable = mkDefault true;
-      nix.enable = mkDefault true;
-      ssh.enable = mkDefault true;
-      users.enable = mkDefault true;
-      xserver.enable = mkDefault true;
-    };
-  };
+  config = mkMerge [
+    (mkIf cfg.enableDefault {
+      my.system = {
+        crypt.enable = mkDefault true;
+        docker.enable = mkDefault true;
+        gui.enable = mkDefault true;
+        locales.enable = mkDefault true;
+        networking.enable = mkDefault true;
+        nix.enable = mkDefault true;
+        ssh.enable = mkDefault true;
+        users.enable = mkDefault true;
+        xserver.enable = mkDefault true;
+      };
+    })
+
+    (mkIf cfg.enableBasic {
+      my.system = {
+        crypt.enable = mkDefault true;
+        docker.enable = mkDefault true;
+        locales.enable = mkDefault true;
+        networking.enable = mkDefault true;
+        nix.enable = mkDefault true;
+        ssh.enable = mkDefault true;
+        users.enable = mkDefault true;
+      };
+    })
+  ];
 }
