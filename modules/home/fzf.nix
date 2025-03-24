f2130bf2b2b290e53921a8bd432bf18d4df0c130
Hugo Moreau <hugo.moreau@epita.fr>
2025-03-24 23:12:59 +0800

    modules: home: fzf: add colors

--- modules/home/fzf.nix
+++ modules/home/fzf.nix
@@ -9,11 +9,23 @@ in
     enable = mkEnableOption "Home fzf configuration";
   };
 
-  config = mkIf cfg.enable {
-    programs.fzf.enable = true;
+  config.programs.fzf = mkIf cfg.enable {
+    enable = true;
+    enableZshIntegration = true;
 
-    home.sessionVariables = {
-      FZF_DEFAULT_OPTS = "--color=fg:#e5e9f0,bg:-1,hl:#81a1c1 --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1 --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b";
+    colors = {
+      fg = "#e5e9f0";
+      bg = "-1";
+      hl = "#81a1c1";
+      "fg+" = "#e5e9f0";
+      "bg+" = "#3b4252";
+      "hl+" = "#81a1c1";
+      info = "#eacb8a";
+      prompt = "#bf6069";
+      pointer = "#b48dac";
+      marker = "#a3be8b";
+      spinner = "#b48dac";
+      header = "#a3be8b";
     };
   };
 }
