7fa965917b558dce99b3a59f6a5de67625d901a4
Hugo Moreau <hugo.moreau@epita.fr>
2025-03-25 10:53:56 +0800

    modules: home: secrets: change secretsDir

--- modules/home/secrets.nix
+++ modules/home/secrets.nix
@@ -1,6 +1,7 @@
 { config, options, inputs, lib, pkgs, ... }:
 
 let
+  xdgCfg = config.my.home.xdg;
 in
 {
   imports = [
@@ -16,7 +17,19 @@ in
     };
   };
 
-  config.age.identityPaths = options.age.identityPaths.default ++ [
-    "${config.home.homeDirectory}/.ssh/agenix"
-  ];
+  config.age = {
+    identityPaths = options.age.identityPaths.default ++ [
+      "${config.home.homeDirectory}/.ssh/agenix"
+    ];
+
+    # We want a concrete path cause agenix use shell variables and therefore
+    # not expanded, so it's difficult to use in non shell configuration.
+    # Since XDG RUNTIME DIR is not user set, better to use something else.
+    secretsDir =
+      if xdgCfg.enable
+      then
+        "${config.xdg.dataHome}/agenix"
+      else
+        options.age.secretsDir.default;
+  };
 }
