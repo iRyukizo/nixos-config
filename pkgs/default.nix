a86b33b7ff6b7b0305dfbfc0828adc959f4320a9
Hugo Moreau <hugo.moreau@epita.fr>
2026-03-10 22:18:34 +0800

    pkgs: remove gopkgsite
    gopkgsite is now packaged since 2024 on unstable

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,7 +8,6 @@ rec {
   system-san-francisco-font = callPackage ./system-san-francisco-font { };
   san-francisco-display-regular-nerd-font = callPackage ./san-francisco-display-regular-nerd-font { };
   san-francisco-font = callPackage ./san-francisco-font { };
-  gopkgsite = callPackage ./gopkgsite { };
 } // lib.optionalAttrs (!stdenv.isDarwin) {
   spotify-song-getter = callPackage ./spotify-song-getter { };
 }
