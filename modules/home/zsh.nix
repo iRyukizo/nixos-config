a7a33d2e50ef81273dc2c1f813ed1fa4eeba7bda
Hugo Moreau <hugo.moreau@epita.fr>
2026-06-23 20:36:17 +0800

    modules: home: zsh: update zsh-nix-shell plugin

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -55,8 +55,8 @@ in
           src = pkgs.fetchFromGitHub {
             owner = "chisui";
             repo = "zsh-nix-shell";
-            rev = "v0.1.0";
-            sha256 = "vUpBkwowQiSmlB/4ZmPlC8794xi9+1kVaQN5BxNk0Go=";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
           };
         }
         {
