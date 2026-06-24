a8986f0f1590ae1a0371e4f323c666d3b7e434ff
Hugo Moreau <hugo.moreau@epita.fr>
2026-06-24 20:25:26 +0800

    modules: nixos: system: add sddm them customization

--- modules/nixos/system/sddm.nix
+++ modules/nixos/system/sddm.nix
@@ -3,9 +3,22 @@
 let
   inherit (lib) mkEnableOption mkIf;
 
-  custom-sddm-astronaut = pkgs.sddm-astronaut.override {
+  custom-sddm-astronaut = (pkgs.sddm-astronaut.override {
     embeddedTheme = "astronaut";
-  };
+    themeConfig = {
+      Background = "Backgrounds/nix-d-nord-blue.jpg";
+      FormPosition = "right";
+    };
+  }).overrideAttrs (oldAttrs: {
+    installPhase = oldAttrs.installPhase + ''
+      chmod u+w $out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/
+      cp ${(pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/OulipianSummer/nixos-pattern-nord-wallpapers/d6334f5b2a83c1072d804fc4750450e18f1a02e4/jpgs/nix-d-nord-blue.jpg";
+        sha256 = "0ambkmjcsbmmjl5hji5sq0bm44xd3scg3gxkjzfrl83dp0a5bx7y";
+      })} \
+      $out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/nix-d-nord-blue.jpg
+    '';
+  });
 
   cfg = config.my.system.sddm;
 in
