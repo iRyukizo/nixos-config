d3d3703853de3be8947540a7b33238f6827928c9
Hugo Moreau <hugo.moreau@epita.fr>
2026-06-27 03:39:43 +0800

    modules: home: vim: neovim: add correct lsp-format

--- modules/home/vim/neovim/plugin/settings/format.lua
+++ modules/home/vim/neovim/plugin/settings/format.lua
@@ -1,4 +1,5 @@
 local lsp_format = require('lsp-format')
+local wk = require('which-key')
 
 lsp_format.setup({})
 
@@ -7,11 +8,23 @@ vim.api.nvim_create_autocmd("LspAttach", {
     group = format,
     callback = function(args)
         local buf, data = args.buf, args.data
-        local ft = vim.bo[buf].filetype
 
-        if ft ~= "c" then
-            local client = assert(vim.lsp.get_client_by_id(data.client_id))
-            lsp_format.on_attach(client, buf)
-        end
+        local client = assert(vim.lsp.get_client_by_id(data.client_id))
+        lsp_format.on_attach(client, buf)
     end,
 })
+
+-- By default disable formatting on save for c language
+lsp_format.disable({ args = "c" })
+
+local function toggle_buffer_format_on_save()
+    local ft = vim.bo.filetype
+    lsp_format.toggle({ args = ft })
+    vim.notify("LSP Format on save for `" .. ft .. "` filetype " .. (lsp_format.disabled_filetypes[ft] and "disabled" or "enabled"))
+end
+
+local keys = {
+    { "<leader>cF", toggle_buffer_format_on_save, desc = "Toggle format code" },
+}
+
+wk.add(keys)
