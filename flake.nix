d3da935ff60a7e17882c3d9071791deda2aa4e96
Hugo Moreau <hugo.moreau@epita.fr>
2026-06-27 18:38:27 +0800

    flake: update system warning

--- flake.nix
+++ flake.nix
@@ -77,7 +77,7 @@
     eachDefaultSystem
       (system:
       let
-        pkgs = nixpkgs.legacyPackages.stdenv.hostPlatform.${system};
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       rec {
         checks = {
