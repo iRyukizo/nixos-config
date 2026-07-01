5be2c60c9f5a38c4b143185944c4c8b145d27af1
Hugo Moreau <hugo.moreau@epita.fr>
2026-07-01 22:55:45 +0800

    modules: home: vim: fix nonempty node in snippets

--- modules/home/vim/neovim/lua/ryuki/snippets/gitcommit.lua
+++ modules/home/vim/neovim/lua/ryuki/snippets/gitcommit.lua
@@ -4,9 +4,7 @@ local s = ls.snippet
 local i = ls.insert_node
 local t = ls.text_node
 local c = ls.choice_node
-local f = ls.function_node
-local d = ls.dynamic_node
-local sn = ls.snippet_node
+local n = require("luasnip.extras").nonempty
 local fmt = require("luasnip.extras.fmt").fmt
 
 return {
@@ -31,19 +29,8 @@ return {
                     t("revert"),
                 }),
                 scope = i(2, "scope"),
-                par1 = d(5, function(args)
-                    if args[1][1] ~= "" then
-                       return sn(nil, { t("(") })
-                    end
-                    return sn(nil, {})
-                end, { 2 }),
-                par2 = d(6, function(args)
-                    if args[1][1] ~= "" then
-                       return sn(nil, { t(")") })
-                    end
-                    return sn(nil, {})
-                end, { 2 }),
-
+                par1 = n(2, "("),
+                par2 = n(2, ")"),
                 excl = c(3, { t(""), t("!") }),
                 desc = i(4, "commit description"),
             }
