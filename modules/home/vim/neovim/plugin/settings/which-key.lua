b1de023d01498db16c73a9c714ffbf1b8bd2c999
Hugo Moreau <hugo.moreau@epita.fr>
2026-07-09 11:33:45 +0800

    modules: home: vim: modify which-key borders

--- modules/home/vim/neovim/plugin/settings/which-key.lua
+++ modules/home/vim/neovim/plugin/settings/which-key.lua
@@ -1,5 +1,12 @@
 local wk = require("which-key")
 
+wk.setup({
+    win = {
+        border = "rounded",
+        no_overlap = true,
+    },
+})
+
 local keys = {
     { "<leader><leader>", vim.cmd.nohlsearch, desc = "Clear search highlight" },
 }
