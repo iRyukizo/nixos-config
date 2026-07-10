6a436b5edd027c49668ddc2bd549e876b5547bcf
Hugo Moreau <hugo.moreau@epita.fr>
2026-07-10 11:20:22 +0800

    modules: home: vim: enable lsp hover rendering

--- modules/home/vim/neovim/plugin/settings/render-markdown.lua
+++ modules/home/vim/neovim/plugin/settings/render-markdown.lua
@@ -2,5 +2,19 @@ local render_markdown = require("render-markdown")
 
 render_markdown.setup({
     enabled = false,
-    overrides = { preview = { enabled = true } },
+    overrides = {
+        preview = {
+            enabled = true,
+        },
+        buflisted = {},
+        buftype = {
+            nofile = {
+                enabled = true,
+                render_modes = true,
+                padding = { highlight = "NormalFloat" },
+                sign = { enabled = false },
+            },
+        },
+        filetype = {},
+    },
 })
