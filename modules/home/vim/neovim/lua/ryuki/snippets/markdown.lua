d8abc2796d5b4d3d21345527ee050fc2ca2e84dd
Hugo Moreau <hugo.moreau@epita.fr>
2026-07-10 10:41:57 +0800

    modules: home: vim: add markdown table snippet

--- modules/home/vim/neovim/lua/ryuki/snippets/markdown.lua
+++ modules/home/vim/neovim/lua/ryuki/snippets/markdown.lua
@@ -7,7 +7,7 @@ local d = ls.dynamic_node
 local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 
-local function make_rtable(args)
+local function make_table(args, prefix, suffix, separator)
     local rows = tonumber(args[1][1]) or 0
     local cols = tonumber(args[2][1]) or 0
 
@@ -16,12 +16,20 @@ local function make_rtable(args)
     end
 
     local nodes = {}
+    local sep = {}
 
     for c = 1, cols do
-        table.insert(nodes, t("|_."))
+        table.insert(nodes, t("|" .. prefix))
         table.insert(nodes, i(c, "col" .. c))
+        table.insert(nodes, t(suffix))
+        if separator then
+            table.insert(sep, " --- ")
+        end
     end
     table.insert(nodes, t("|"))
+    if separator then
+        table.insert(nodes, t({ "", table.concat(sep, "|") .. "|" }))
+    end
 
     for _ = 1, rows do
         table.insert(nodes, t({ "", "" }))
@@ -33,6 +41,14 @@ local function make_rtable(args)
     end
 
     return sn(nil, nodes)
+end
+
+local function make_rtable(args)
+    return make_table(args, "_.", "", false)
+end
+
+local function make_mtable(args)
+    return make_table(args, " ", " ", true)
 end
 
 return {
@@ -57,5 +73,13 @@ return {
         i(2, "4"),
         t({ "-->", "" }),
         d(3, make_rtable, { 1, 2 }),
+    }),
+    s("table", {
+        t("<!-- rows: "),
+        i(1, "3"),
+        t(" , cols: "),
+        i(2, "4"),
+        t({ "-->", "" }),
+        d(3, make_mtable, { 1, 2 }),
     }),
 }
