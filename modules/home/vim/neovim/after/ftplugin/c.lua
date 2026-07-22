be56a066532011c34dce8f4d74fcfe3cd97638ca
Hugo Moreau <hugo.moreau@epita.fr>
2026-07-22 08:59:57 +0800

    modules: home: vim: add c file custom command

--- modules/home/vim/neovim/after/ftplugin/c.lua
+++ modules/home/vim/neovim/after/ftplugin/c.lua
@@ -3,3 +3,9 @@ local cf = require("ryuki.clang-format")
 vim.opt_local.comments = "s0:/*,mb:**,ex:*/"
 
 cf.set_cc()
+
+vim.api.nvim_buf_create_user_command(0, "CCReload", function()
+    cf.reload()
+end, {
+    desc = "Reload Colomn Color config from clang-format",
+})
