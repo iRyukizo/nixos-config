771665fad5a32b08cc78b8fce95b3dbed4dceead
Hugo Moreau <hugo.moreau@epita.fr>
2026-07-22 08:18:13 +0800

    modules: home: vim: add clang-format options

---
+++ modules/home/vim/neovim/lua/ryuki/clang-format.lua
@@ -0,0 +1,53 @@
+local M = {}
+
+--- Read ColumnLimit from `.clang-format` file
+--- @param filepath string `.clang-format` filepath
+--- @return integer? colorcolumn value
+local function get_cf_cc(filepath)
+    local file = io.open(filepath, "r")
+    if not file then
+        return nil
+    end
+
+    for line in file:lines() do
+        local limit = line:match("^%s*ColumnLimit:%s*(%d+)")
+        if limit then
+            file:close()
+            return tonumber(limit)
+        end
+    end
+
+    file:close()
+    return nil
+end
+
+--- Search upward for `.clang-format` or `_clang-format`
+--- @return string? `clang-format` filepath
+local function find_cf()
+    local cwd = vim.fn.expand("%:p:h")
+    local paths = { ".clang-format", "_clang-format" }
+
+    while cwd and cwd ~= "/" do
+        for _, name in ipairs(paths) do
+            local candidate = cwd .. "/" .. name
+
+            if vim.fn.filereadable(candidate) == 1 then
+                return candidate
+            end
+        end
+        cwd = vim.fn.fnamemodify(cwd, ":h")
+    end
+    return nil
+end
+
+--- Apply colorcolumn based on root folders' `.clang-format` ColumnLimit
+function M.set_cf_cc()
+    local config_file = find_cf()
+    local limit = config_file and get_cf_cc(config_file) or nil
+
+    if limit and limit > 0 then
+        vim.opt_local.colorcolumn = tostring(limit)
+    end
+end
+
+return M
