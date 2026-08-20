73859342e340fc6d853dcdaaf61fabad32bc32ef
Hugo Moreau <hugo.moreau@epita.fr>
2026-08-20 13:52:39 +0800

    modules: home: vim: add printf injections for c

---
+++ modules/home/vim/neovim/after/queries/c/injections.scm
@@ -0,0 +1,9 @@
+; extends
+
+(
+ (call_expression
+   function: (identifier) @_name
+   arguments: (argument_list . (string_literal . (string_content) @injection.content)))
+ (#match? @_name "_*[pP]rintf$|_FORMAT$|_PRINTF$")
+ (#set! injection.language "printf")
+)
