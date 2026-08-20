30081c3a74b198f40d573a049df10ef05268e124
Hugo Moreau <hugo.moreau@epita.fr>
2026-08-20 14:16:42 +0800

    modules: home: vim: add c printf corner case

--- modules/home/vim/neovim/after/queries/c/injections.scm
+++ modules/home/vim/neovim/after/queries/c/injections.scm
@@ -3,7 +3,15 @@
 (
  (call_expression
    function: (identifier) @_name
-   arguments: (argument_list . (string_literal . (string_content) @injection.content)))
+   arguments: (argument_list (string_literal (string_content) @injection.content)))
+ (#match? @_name "_*[pP]rintf$|_FORMAT$|_PRINTF$")
+ (#set! injection.language "printf")
+)
+
+(
+ (call_expression
+   function: (identifier) @_name
+   arguments: (argument_list (concatenated_string (string_literal (string_content) @injection.content))))
  (#match? @_name "_*[pP]rintf$|_FORMAT$|_PRINTF$")
  (#set! injection.language "printf")
 )
