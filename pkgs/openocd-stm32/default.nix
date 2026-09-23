abb3a283c5e3b1edb485f999bd2e1f28cbc78e00
Hugo Moreau <hugo.moreau@epita.fr>
2026-09-23 14:09:07 +0800

    pkgs: openocd-stm32: add support for stm32c5 mcu

--- pkgs/openocd-stm32/default.nix
+++ pkgs/openocd-stm32/default.nix
@@ -1,6 +1,7 @@
 { openocd
 , autoreconfHook
 , fetchFromGitHub
+, fetchurl
 ,
 }:
 
@@ -19,6 +20,21 @@ openocd.overrideAttrs (
     nativeBuildInputs = old.nativeBuildInputs ++ [
       autoreconfHook
     ];
+
+    patches = [
+      ./001-stm32c5-add-xldr.patch
+    ];
+
+    postInstall = old.postInstall + ''
+      mkdir -p $out/share/openocd/scripts/flash
+      cp ${fetchurl {
+        url = "https://raw.githubusercontent.com/STMicroelectronics/stm32c5xx-dfp/main/Flash/STM32C5%5B56%5Dx.xldr";
+        hash = "sha256-ExeEwuTrRYkgC2FOVrg6vSGjSQbB/Uhco9gGN40ocy0=";
+      }} "$out/share/openocd/scripts/flash/STM32C5[56]x.xldr"
+      substituteInPlace $out/share/openocd/scripts/target/stm32c5x.cfg \
+        --replace-fail 'flash/STM32C5\[56\]x.xldr' $out'/share/openocd/scripts/flash/STM32C5\[56\]x.xldr'
+    '';
+
 
     meta = openocd.meta // {
       description = "STMicroelectronics fork of OpenOCD";
